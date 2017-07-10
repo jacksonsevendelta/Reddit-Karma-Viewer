0622b70b87a7c5f1a69e0e5fa62b7855c1c2802c
Jackson Seven Delta <jacksonsevendelta@gmail.com>
2017-07-10 19:52:03 +1000

    Create karmaviewer.py

---
+++ karmaviewer.py
@@ -0,0 +1,27 @@
+try: input = raw_input # checks to see if in Python 3x or 2x to get input() command
+except NameError: pass
+
+localclientid = '<YOUR CLIENT ID HERE>' # see readme.md for info on how to set this up
+localclientsecret = '<YOUR CLIENT SECRET HERE>'  # see readme.md for info on how to set this up
+
+import praw #Makes sure we can use the module
+#The next part will tell reddit that we are who we are, so that we can
+#grab info from accounts.
+reddit = praw.Reddit(client_id=localclientid,
+                     client_secret=localclientsecret,
+                     user_agent='RPi Karma Viewer') # This can be the title of the project
+
+me = reddit.redditor(input("Enter a reddit username: ")) #Defines the reddit username
+linkkarma = me.link_karma #Defines the variable linkkarma as the username's link karma
+commentkarma = me.comment_karma #Defines the variable commentkarma as the username's comment karma
+totalkarma = linkkarma + commentkarma #This will make a variable called totalkarma that will add both of the karma variables together.
+
+print "Reddit username karma viewer (RAKV) Version 1.1"
+print "Script available at https://github.com/jacksonsevendelta/Reddit-Karma-Viewer"
+print "You selected reddit username u/{}.".format(me)
+print "TOTAL KARMA:",totalkarma
+print "LINK KARMA:",linkkarma
+print "COMMENT KARMA:",commentkarma
+print "Finished for username",me
+# print "Additionally, you can find this username's reddit page at https://reddit.com/u/
+print "Additionally, you can find this user's page at https://reddit.com/u/{}.".format(me)
